b685cafc9156742ab95e9d8b0f8c13786b3dfcd5
Administrator <beforenight@163.com>
2016-07-04 20:56:10 +0800

    单例模式的基本实现

---
+++ src/com/bubble/test/Singleton.java
@@ -0,0 +1,17 @@
+package com.bubble.test;
+
+/**
+ * Created by Administrator on 2016/7/4.
+ */
+public class Singleton {
+    private Singleton() {
+        System.out.println("Singleton is create");
+    }
+
+    private static Singleton singleton = new Singleton();
+
+    //单例模式
+    public static Singleton getSingleton() {
+        return singleton;
+    }
+}
