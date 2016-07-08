f55eb6ea68d7b8e5d7691a6d93d794887051e70c
Administrator <beforenight@163.com>
2016-07-08 10:38:36 +0800

    Android GPS坐标距离计算

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Java性能优化
 ###2.BinarySearch.java 针对int类型数组的二分查找算法
 ###3.Singleton.java 单例模式
 ###4.LazySingleton.java 延迟加载的单例模式代码
-###5.MainActivity.java 查询获得历史轨迹后，可结合百度地图Android SDK绘制并展示轨迹。
-###5.SortDemo.java  Arrays.sort(arr)默认升序排列。
+###5.MainActivity.java 查询获得历史轨迹后，可结合百度地图Android SDK绘制并展示轨迹
+###5.SortDemo.java  Arrays.sort(arr)默认升序排列
+###5.Computer2GpsPointDistance.java  Android GPS坐标距离计算
 
---
+++ src/com/bubble/test/Computer2GpsPointDistance.java
@@ -0,0 +1,23 @@
+package com.bubble.test;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ * Android GPS坐标距离计算
+ */
+public class Computer2GpsPointDistance {
+//    LatLng latLng1 = new LatLng(37.014232, 117.565597);
+//    LatLng latLng2 = new LatLng(31.137012, 121.345326);
+//    double mDistance = DistanceUtil.getDistance(latLng1, latLng2);
+//    Log.e(TAG, "onCreate: 百度-距离：" + mDistance + "米");
+//
+//    Location location1 = new Location("l1");
+//    location1.setLatitude(37.014232);
+//    location1.setLongitude(117.565597);
+//    Location location2 = new Location("l2");
+//    location2.setLatitude(31.137012);
+//    location2.setLongitude(121.345326);
+//
+//    07-08 10:17:47.678 27561-27561/baidumapsdk.demo E/MainActivity: onCreate: 百度-距离：740275.3203764297米
+//    07-08 10:17:47.678 27561-27561/baidumapsdk.demo E/MainActivity: onCreate: 谷歌-距离：740275.3203764297米
+
+}
